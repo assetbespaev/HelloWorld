ada263085663202d86747fceb8d14fbfecf823da
assetbespaev <asset.bespaev@gmail.com>
2014-10-02 12:00:39 +0600

    System.out.println("Hello Wolrd 2 !!!");

--- HelloWorld/com.git.example]/HelloWorld.java
+++ HelloWorld/com.git.example]/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello Wolrd 1 !!!");
     System.out.println("Hello Wolrd 2 !!!");
+    System.out.println("Hello Wolrd 3 !!!");
   }
   
 }
