91eb0e3db632f5c49801c81bd0f3e56fe49f1df9
assetbespaev <asset.bespaev@gmail.com>
2014-10-02 11:24:18 +0600

    Modify GitTest class!

--- HelloWorld/src/com/git/example/GitTest.java
+++ HelloWorld/src/com/git/example/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
   
   public static void main(String[] args) {
     System.out.println("Hello World From GitTest class!!");
+    System.out.println("Edit new changes!");
   }
   
 }
