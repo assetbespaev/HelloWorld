049d5cd0d47c5b5c6d7e555ef5584963d6a666a1
assetbespaev <asset.bespaev@gmail.com>
2014-10-02 11:29:54 +0600

    resolve conflicts!

--- HelloWorld/src/com/git/example/GitTest.java
+++ HelloWorld/src/com/git/example/GitTest.java
@@ -4,7 +4,8 @@ public class GitTest {
   
   public static void main(String[] args) {
     System.out.println("Hello World From GitTest class!!");
-    System.out.println("Edit new changes!");
+    System.out.println("Edit new changes");
+    System.out.println("Git is modified!");
   }
   
 }
