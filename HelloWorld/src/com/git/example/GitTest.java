b10cbdb638f966671eae2452b7c8e73afd897f96
assetbespaev <asset.bespaev@gmail.com>
2014-10-02 21:58:51 +0600

    Create println(msg) method in GitTest!

--- HelloWorld/src/com/git/example/GitTest.java
+++ HelloWorld/src/com/git/example/GitTest.java
@@ -1,13 +1,17 @@
 package com.git.example;
 
 public class GitTest {
-  
-  public static void main(String[] args) {
-    System.out.println("Hello World From GitTest class!!");
-    System.out.println("Edit new changes");
-    System.out.println("Git is modified!");
-    System.out.println("Hi, GreetGo!");
-    System.out.println("Hi, guys!");
-  }
-  
+
+	public static void main(String[] args) {
+		println("Hello World From GitTest class!!");
+		println("Edit new changes");
+		println("Git is modified!");
+		println("Hi, GreetGo!");
+		println("Hi, guys!");
+	}
+
+	private static void println(String msg) {
+		System.out.println(msg);
+	}
+
 }
