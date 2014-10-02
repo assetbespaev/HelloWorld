18b067e9853dee9e14b412e1eefef07e9f624c29
assetbespaev <asset.bespaev@gmail.com>
2014-10-02 22:11:27 +0600

    ssss

--- HelloWorld/com.git.example]/HelloWorld.java
+++ HelloWorld/com.git.example]/HelloWorld.java
@@ -1,7 +1,9 @@
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello Wolrd 1 !!!");
-    System.out.println("Hello Wolrd 2 !!!");
-    System.out.println("Hello  Mike Will Made!!!");
-  }
+	public static void main(String[] args) {
+		System.out.println("Hello Wolrd 1 !!!");
+		System.out.println("Hello Wolrd 2 !!!");
+		System.out.println("Hello  Mike Will Made!!!");
+		System.out.println("Ariana Grande!!!");
+		System.out.println("Indila!!!");
+	}
 }
