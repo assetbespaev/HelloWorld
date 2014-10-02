1ac7cb8693a2786bc18d3366ba83960599e5e191
assetbespaev <asset.bespaev@gmail.com>
2014-10-02 21:53:17 +0600

    Update GitTest.java
    create println() method in GitTest class;

--- HelloWorld/src/com/git/example/GitTest.java
+++ HelloWorld/src/com/git/example/GitTest.java
@@ -3,6 +3,10 @@ package com.git.example;
 public class GitTest {
   
   public static void main(String[] args) {
+    println();
+  }
+  
+  public static void println(){
     System.out.println("Hello World From GitTest class!!");
     System.out.println("Edit new changes");
     System.out.println("Git is modified!");
